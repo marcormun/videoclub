5b03a360b054dfb6f17e3062c0bbd44306558d27
shencanet <santi.dionis@gmail.com>
2022-06-26 17:43:18 +0200

    test user delete users plural

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,11 +2,11 @@ const router = require('express').Router();
 const userController = require('../controllers/userController');
 
 
-router.get('/user', userController.getAll);
-router.post('/user', userController.createUser);
-router.get('/user/:id', userController.getUserById);
-router.put('/user/:id', userController.update);
-router.delete('/user/:id', userController.delete);
+router.get('/users', userController.getAll);
+router.post('/users', userController.createUser);
+router.get('/users/:id', userController.getUserById);
+router.put('/users/:id', userController.update);
+router.delete('/users/:id', userController.delete);
 
 
 module.exports = router;
