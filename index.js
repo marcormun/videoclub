c953ede9eb8aaff78ebc030ef6038be91610e636
marcormun <mcordontrabajo@gmail.com>
2022-06-26 15:55:43 +0200

    feat: First commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+require('dotenv').config();
+const db = require('./config/database');
+
+const app = express();
+
+//middleware
+app.use(express.json())
+
+//Port
+const PORT = process.env.PORT || 4000;
+
+//routes
+
+
+app.get('/', (req, res)=> {
+    return res.send('Bienvenidos al videoclub');
+});
+
+app.get('*',(req,res)=>{
+    return res.status(404).send('404 route not found')
+})
+
+//connect database
+
+db().then(()=>{
+app.listen(PORT, ()=> console.log('Server is running in ' + PORT));
+}).catch((error)=>{
+    console.log('Error connecting to mongoDB ', error);
+})
