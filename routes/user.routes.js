d92a82855e2a5ac5811049285bfca2ea20d4b463
shencanet <santi.dionis@gmail.com>
2022-06-26 16:36:57 +0200

    fixed inded

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,8 +2,8 @@ const router = require('express').Router();
 const userController = require('../Controllers/userController');
 
 
-router.get('/user', verifyToken, userController.getAll);
-router.post('/user', verifyToken, userController.createUser);
+router.get('/user', userController.getAll);
+router.post('/user', userController.createUser);
 //router.get('/user/:id', verifyToken, userController.getUserById);
 //router.put('/user/:id', verifyToken, userController.update);
 //router.delete('/user/:id', verifyToken, userController.delete);
