29a196d6f05e1795a44c98a6f6db85ecfc89a9bc
marcormun <mcordontrabajo@gmail.com>
2022-06-26 17:06:09 +0200

    feat: solved merge

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -24,4 +24,36 @@ userController.getAll = async (req, res) => {
     }
 };
 
+<<<<<<< HEAD
+=======
+userController.createUser = async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+
+        const newUser = {
+            name,
+            email,
+            password
+        };
+
+        await User.create(newUser);
+
+        return res.status(200).json(
+            {
+                success: true,
+                message: 'Create user successfully'
+            }
+        )
+    } catch (error) {
+        return res.status(500).json(
+            {
+                success: false,
+                message: 'Error creating user',
+                error: error?.message || error
+            }
+        )
+    }
+};
+
+>>>>>>> 41a1b9417e1db245ebd4e1005220e04c54692170
 module.exports = userController;
