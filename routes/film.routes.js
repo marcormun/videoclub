dff271a7997eb1af47e593787fc2ff9d6dc9eeef
marcormun <mcordontrabajo@gmail.com>
2022-06-26 17:52:03 +0200

    feat:getfilmbyid added

--- routes/film.routes.js
+++ routes/film.routes.js
@@ -3,5 +3,6 @@ const filmController = require('../controllers/filmController');
 
 router.get('/films',filmController.getAll);
 router.post('/films', filmController.addFilm);
+router.get('/films/:id',filmController.getFilmById);
 
 module.exports = router;
