54d6f2d072fad16cc1a90c9310750a5f5015e9f0
shencanet <santi.dionis@gmail.com>
2022-06-26 17:12:16 +0200

    fixed commit

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -53,4 +53,47 @@ userController.createUser = async (req, res) => {
     }
 };
 
+userController.getUserById = async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const user = await User.findById(id);
+
+        if(!user) {
+            return res.status(404).json(
+                {
+                    success: true,
+                    message: "User not found",
+                    data: []
+                }
+            ); 
+        }
+
+        return res.status(200).json(
+            {
+                success: true,
+                message: "User found",
+                data: user
+            }
+        );
+    } catch (error) {
+        if(error?.message.includes('Cast to ObjectId failed')) {
+            return res.status(404).json(
+                {
+                    success: true,
+                    message: "User not found"
+                }
+            );
+        };
+
+        return res.status(500).json(
+            {
+                success: false,
+                message: "Error finding user",
+                error: error?.message || error
+            }
+        );
+    }
+};
+
 module.exports = userController;
