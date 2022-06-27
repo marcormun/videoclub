cc438ae5c09270440a959a16db1fa76b6ca0861f
shencanet <santi.dionis@gmail.com>
2022-06-27 17:19:11 +0200

    is admin validacion

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
 const userController = require('../controllers/userController');
+const isAdmin = require('../middlewares/isAdmin');
 
 
-//router.get('/users', userController.getAll);
+router.get('/users', isAdmin, userController.getAll);
 router.post('/users', userController.createUser);
 router.get('/users/:id', userController.getUserById);
 router.put('/users/:id', userController.update);
