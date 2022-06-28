56ea076f3e7ca7f654883c2de96afdf961f1be52
marcormun <mcordontrabajo@gmail.com>
2022-06-28 22:05:01 +0200

    fix: filmRoutes

--- routes/filmRoutes.js
+++ routes/filmRoutes.js
@@ -1,9 +1,5 @@
 const router = require('express').Router();
-<<<<<<< HEAD
-const filmController = require('../controllers/FilmController');
-=======
 const filmController = require('../controllers/FilmController')
->>>>>>> ec463c3b7fc3f4f4e4f1d5d2c79ea71c3019f010
 const isAdmin = require('../middlewares/isAdmin');
 const verifyToken = require('../middlewares/verifyToken');
 
