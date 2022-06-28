d64c3ea800773d90ea5823dca202f1cdb5d51d6a
marcormun <mcordontrabajo@gmail.com>
2022-06-28 21:37:40 +0200

    fix: Controllers folder name changed

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,4 +1,4 @@
-const authController = require('../controllers/AuthController');
+const authController = require('../Controllers/AuthController');
 const verifyToken = require('../middlewares/verifyToken');
 
 const router = require('express').Router();
--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const orderController = require('../controllers/OrderController');
+const orderController = require('../Controllers/OrderController');
 const isAdmin = require('../middlewares/isAdmin');
 const verifyToken = require('../middlewares/verifyToken');
 
